494d761455eefca3916eb1371b3fa09f7ea089b4
mignonstyle <mignonxstyle@gmail.com>
2016-10-15 18:05:22 +0900

    Modify gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 // $ npm run build
 
 var gulp         = require( 'gulp' );
-var path         = require('path');
+var path         = require( 'path' );
 var changed      = require( 'gulp-changed' );
 var concat       = require( 'gulp-concat' );
 var csso         = require( 'gulp-csso' );
@@ -26,9 +26,6 @@ var paths = {
 	"jsSrc" : "./src/js/**/*.js",
 	"jsDir" : "./js/",
 
-	//"imgSrc" : "./src/images/**",
-	//"imgDir" : "./images/",
-
 	// admin widget paths
 	"admin_scssSrc" : "./src/admin/admin-widget/*.scss",
 
@@ -37,9 +34,6 @@ var paths = {
 
 	"widget_jsSrc" : "./src/admin/admin-widget/widget-js/**/*.js",
 	"widget_jsDir" : "./admin/widget/widget-js/",
-
-	//"widget_imgSrc" : "./src/admin/admin-widget/widget-images/**/**",
-	//"widget_imgDir" : "./admin/widget/widget-images/",
 }
 
 // tasks
@@ -53,15 +47,15 @@ gulp.task( 'browser-sync', function() {
 
 gulp.task( 'scss', function() {
 	return gulp.src( paths.scssSrc )
-		.pipe( $.sass() ).on( 'error', $.sass.logError )
-		.pipe( $.autoprefixer( {
+		.pipe( sass() ).on( 'error', sass.logError )
+		.pipe( autoprefixer( {
 			browsers: ['last 2 versions']
 		} ) )
 	.pipe( gulp.dest( paths.scssDir ) )
-	.pipe( $.rename( {
+	.pipe( rename( {
 		suffix: '.min'
 	} ) )
-	.pipe( $.csso() )
+	.pipe( csso() )
 	.pipe( gulp.dest( paths.scssDir ) )
 	.pipe( browserSync.reload( {
 		stream : true,
@@ -71,15 +65,15 @@ gulp.task( 'scss', function() {
 
 gulp.task( 'widget-scss', function() {
 	return gulp.src( paths.widget_scssSrc )
-		.pipe( $.sass() ).on( 'error', $.sass.logError )
-		.pipe( $.autoprefixer( {
+		.pipe( sass() ).on( 'error', sass.logError )
+		.pipe( autoprefixer( {
 			browsers: ['last 2 versions']
 		} ) )
 	.pipe( gulp.dest( paths.widget_scssDir ) )
-	.pipe( $.rename( {
+	.pipe( rename( {
 		suffix: '.min'
 	} ) )
-	.pipe( $.csso() )
+	.pipe( csso() )
 	.pipe( gulp.dest( paths.widget_scssDir ) )
 	.pipe( browserSync.reload( {
 		stream : true,
@@ -91,38 +85,17 @@ gulp.task( 'bs-reload', function() {
 	browserSync.reload();
 } );
 
-/*
-gulp.task( 'image', function() {
-	return gulp.src( paths.imgSrc )
-		.pipe( $.changed( paths.imgDir ) )
-		.pipe( $.imagemin( {
-			optimizationLevel: 3
-		} ) )
-		.pipe( gulp.dest( paths.imgDir ) );
-} );
-*/
-/*
-gulp.task( 'widget-image', function() {
-	return gulp.src( paths.widget_imgSrc )
-		.pipe( $.changed( paths.widget_imgDir ) )
-		.pipe( $.imagemin( {
-			optimizationLevel: 3
-		} ) )
-		.pipe( gulp.dest( paths.widget_imgDir ) );
-} );
-*/
-
 gulp.task( 'js', function() {
 	return gulp.src( paths.jsSrc )
-		.pipe( $.concat( 'main.js', {newLine: '\n'} )
+		.pipe( concat( 'main.js', {newLine: '\n'} )
 	)
 	.pipe( gulp.dest( paths.jsDir ) );
 } );
 
 gulp.task( 'js-min', function() {
 	return gulp.src( paths.jsSrc )
-		.pipe( $.uglify( {preserveComments: 'license'} ) )
-		.pipe( $.concat( 'main.min.js', {newLine: '\n'} )
+		.pipe( uglify( {preserveComments: 'license'} ) )
+		.pipe( concat( 'main.min.js', {newLine: '\n'} )
 	)
 	.pipe( gulp.dest( paths.jsDir ) );
 } );
@@ -134,51 +107,33 @@ gulp.task( 'widget-js', function() {
 
 gulp.task( 'widget-js-min', function() {
 	return gulp.src( paths.widget_jsSrc )
-		.pipe( $.uglify( {preserveComments: 'license'} ) )
-		.pipe( $.rename( {
+		.pipe( uglify( {preserveComments: 'license'} ) )
+		.pipe( rename( {
 			suffix: '.min'
 		} ) )
 		.pipe( gulp.dest( paths.widget_jsDir ) );
 } );
 
 gulp.task( 'default', ['browser-sync', 'scss', 'widget-scss', 'widget-js', 'widget-js-min', 'bs-reload'], function() {
-    $.watch( [paths.phpSrc], function( e ) {
+    watch( [paths.phpSrc], function( e ) {
 		gulp.start( 'bs-reload' )
 	} );
-    $.watch( [paths.scssSrc, paths.admin_scssSrc ], function( e ) {
+    watch( [paths.scssSrc, paths.admin_scssSrc ], function( e ) {
 		gulp.start( 'scss' )
 	} );
-	$.watch( [paths.widget_scssSrc], function( e ) {
+	watch( [paths.widget_scssSrc], function( e ) {
 		gulp.start( 'widget-scss' )
 	} );
-    $.watch( [paths.jsSrc], function( e ) {
+    watch( [paths.jsSrc], function( e ) {
 		gulp.start( 'js' )
 	} );
-	$.watch( [paths.jsSrc], function( e ) {
+	watch( [paths.jsSrc], function( e ) {
 		gulp.start( 'js-min' )
 	} );
-    $.watch( [paths.widget_jsSrc], function( e ) {
+    watch( [paths.widget_jsSrc], function( e ) {
 		gulp.start( 'widget-js' )
 	} );
-	$.watch( [paths.widget_jsSrc], function( e ) {
+	watch( [paths.widget_jsSrc], function( e ) {
 		gulp.start( 'widget-js-min' )
 	} );
 } );
-
-/*
-gulp.task( 'default', ['image', 'widget-image', , , , ], function() {
-
-	$.watch( [paths.imgSrc], function( e ) {
-		gulp.start( 'image' )
-	} );
-	$.watch( [paths.widget_imgSrc], function( e ) {
-		gulp.start( 'widget-image' )
-	} );
-*/
-	/*
-
-*/
-/*
-
-} );
-*/
