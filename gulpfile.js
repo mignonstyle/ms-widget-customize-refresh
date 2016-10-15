f500302dfe9b03771f70e48dfa870fd90b82a48e
mignonstyle <mignonxstyle@gmail.com>
2016-10-15 19:04:45 +0900

    Mdigy gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -49,18 +49,20 @@ gulp.task( 'browser-sync', function() {
 
 gulp.task( 'scss', function() {
 	return gulp.src( paths.scssSrc )
-		.pipe( sass() ).on( 'error', sass.logError )
+		.pipe( sass() )
+        /*
 		.pipe( autoprefixer( {
 			browsers: ['last 2 versions']
 		} ) )
+        */
         /*
 	.pipe( gulp.dest( paths.scssDir ) )
+    */
 	.pipe( rename( {
 		suffix: '.min'
 	} ) )
-    */
-	//.pipe( csso() )
-	//.pipe( gulp.dest( paths.scssDir ) )
+	.pipe( csso() )
+	.pipe( gulp.dest( paths.scssDir ) )
     /*
 	.pipe( browserSync.reload( {
 		stream : true,
